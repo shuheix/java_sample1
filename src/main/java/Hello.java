ee9df407ec2bd9e3eb287ea1ca2438edd99619d1
shuheix <ono.shuhei210@gmail.com>
2024-10-26 22:03:15 +0900

    feat: fizzbuzz

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,14 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        var a = 15;
+        if(a % 15 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (a % 5 == 0) {
+            System.out.println("Fizz");
+        } else if (a % 3 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(a);
+        }
     }
 }
